fde9829d7d6cdaa821e970b4a50cd9874a76a508
Francis Ramirez <Francis Ramirez@LAPTOP-DF01N7UN>
2023-09-26 20:24:54 -0400

    Agregando el StudentController.

---
+++ School/School.Api/Controllers/StudentController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using School.Domain.Entities;
+using School.Domain.Repository;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace School.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentController : ControllerBase
+    {
+        private readonly IStudentRepository studentRepository;
+
+        public StudentController(IStudentRepository studentRepository)
+        {
+            this.studentRepository = studentRepository;
+        }
+        // GET: api/<StudentController>
+        [HttpGet]
+        public IEnumerable<Student> Get()
+        {
+            var students = this.studentRepository.GetStudents();
+            return students;
+        }
+
+        // GET api/<StudentController>/5
+        [HttpGet("{id}")]
+        public Student Get(int id)
+        {
+            return this.studentRepository.GetStudent(id);
+        }
+
+        // POST api/<StudentController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<StudentController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<StudentController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
--- School/School.Api/Program.cs
+++ School/School.Api/Program.cs
@@ -1,6 +1,22 @@
+using Microsoft.EntityFrameworkCore;
+using School.Domain.Repository;
+using School.Infrastructure.Context;
+using School.Infrastructure.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+// Agregar dependencia del contexto //
+
+builder.Services.AddDbContext<SchoolContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("SchoolContext")));
+//Dependencias de los repositorios //
+
+builder.Services.AddTransient<IStudentRepository, StudentRepository>();
+
+
+//Dependencias de los app servicies //
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
